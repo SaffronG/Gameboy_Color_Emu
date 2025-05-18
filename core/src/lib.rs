fc3dc56d23c5882ab3279133d2be4a7b08eb407b
SaffronG <saffrongamer167@gmail.com>
2025-05-17 22:04:58 -0600

    Added more instructions, as well as began implementing new functions for structs

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -26,12 +26,36 @@ enum PpuMode {
     Drawing = 3, // Transfers pixels to the LCD
 }
 
+#[repr(usize)]
+enum Reg {
+    A = 0,
+    B = 1,
+    C = 2,
+    D = 3,
+    E = 4,
+    F = 5,
+    H = 6,
+    L = 7,
+}
+
+#[repr(usize)]
+enum Regu16 {
+    AF = 0,
+    BC = 1,
+    DE = 2,
+    HL = 3,
+}
+
+fn as_u16(d: (u8, u8)) -> u16 {
+    (((0 as u16) & (d.0 as u16)) << 4) & d.1 as u16
+}
+
 pub struct Cpu {
     // 8Bit: General Purpose Registers
     reg_8b: [u8; 8], // a, b, c, d, e, f, h, l
                      
     // 16Bit: General Purpose Registers
-    reg_16b: [(u8,u8); 8],
+    reg_16b: [(u8,u8); 4],
         //af: u16, // Hi - a
         //bc: u16, // Hi - B, Lo - C
         //de: u16, // Hi - D, Lo - E
@@ -55,9 +79,33 @@ pub struct Cpu {
 }
 
 impl Cpu {
-    
+    fn new() -> Cpu {
+        Cpu {
+            reg_8b: [0; 8],
+            reg_16b: Self::init_regs(),
+            sp: 0, // SET TO CORRECT INITIAL VALUE LATER
+            pc: 0,
+            zero: 0,
+            sub: 0,
+            h_carry: 0,
+            carry: 0,
+            ram: [0; RAM_SIZE],
+            hram: [0; HRAM_SIZE],
+            vram: [0; VRAM_SIZE],
+            viewport: [0; WINDOW_SIZE * WINDOW_SIZE],
+            window: [0; WINDOW_SIZE * WINDOW_SIZE],
+            screen: [false; SCREEN_WIDTH * SCREEN_HEIGHT],
+            ppu: Ppu::new(),
+            cartridge: None,
+        }
+    }
+    fn init_regs() -> [(u8,u8); 4] {
+        [(0,0); 4]
+    }
     fn execute(&mut self, op: u8) {
        let opcode: u8 = self.ram[self.pc as usize] & 0xFF;
+       let r16_mask: u8 = 0xCF;
+       let r8_mask: u8 = 0b00111000;
        match opcode {
             0x00 => {
                 // NOP
@@ -71,6 +119,46 @@ impl Cpu {
                 dest.0 = self.ram[self.pc.wrapping_add(1) as usize];
                 dest.1 = self.ram[self.pc.wrapping_add(1) as usize];
             }
+            0x02 => {
+                // Ld [BC], A
+                self.ram[as_u16(self.reg_16b[(op & 0xCF >> 4) as usize]) as usize] = self.ram[self.pc.wrapping_add(1) as usize];
+            },
+            0x03 => {
+                // INC BC
+                let reg = self.reg_16b[(op & r16_mask >> 4) as usize];
+                match reg.0.checked_add(1) {
+                    Some(_) => (),
+                    None => { let _ = reg.0.checked_add(1); },
+                }
+            }
+            0x04 => {
+                // INC B
+                let reg = self.reg_8b[(op & r8_mask >> 3) as usize];
+                match reg.checked_add(1) {
+                    Some(_) => (),
+                    None => { let _ = reg.checked_add(1); },
+                }
+            },
+            0x05 => {
+                // DEC B
+                let reg = self.reg_8b[(op & r8_mask >> 3) as usize];
+                match reg.checked_sub(1) {
+                    Some(_) => (),
+                    None => { let _ = reg.checked_sub(1); },
+                }
+            },
+            0x06 => {
+                // LD B n8
+                let mut reg = self.reg_8b[(op & r8_mask >> 3) as usize];
+                reg = self.ram[self.pc.wrapping_add(1) as usize];
+            },
+            0x07 => {
+                // RLCA
+
+            },
+            0x08 => {
+                // LD [a16], SP
+            },
             _ => todo!(),
        }
     }
@@ -86,6 +174,24 @@ struct Ppu {
         // Background Priority -> Only relevant for sprites, keeps the value of bit 7
 }
 
-struct Cartridge {
+impl Ppu {
+    fn new() -> Ppu {
+        Ppu {
+            oam: [0; OAM_SIZE],
+        }
+    }
+}
 
+struct Cartridge {
+   memory: Vec<u8>, 
+}
+
+impl Cartridge {
+    fn read(&mut self, fname: String) -> std::io::Result<()> {
+        let std_fd = std::fs::File::open(fname)?;
+        match std_fd.read_to_end(&mut self.memory) {
+            Ok(_) => Ok(()),
+            Err(e) => Err(()),
+        }
+    }
 }
